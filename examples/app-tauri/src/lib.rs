06a4caac27a783d78393797cb50be38932bf97c3
nanoqsh <nanoqsh@gmail.com>
2025-03-20 02:20:46 +0500

    doc: update example app

--- examples/app-tauri/src/lib.rs
+++ examples/app-tauri/src/lib.rs
@@ -1,7 +1,32 @@
+use tauri::Window;
+
+#[tauri::command]
+fn hello(message: String) -> String {
+    println!("message from frontend received: {message}");
+    "back!".to_owned()
+}
+
+#[tauri::command]
+fn close(window: Window) {
+    _ = window.close();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                use tauri::Manager;
+
+                app.get_webview_window("app")
+                    .expect("the app webview should exist")
+                    .open_devtools();
+            }
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![hello, close])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- examples/app/src/lib.rs
+++ examples/app/src/lib.rs
@@ -1,10 +1,35 @@
 use {gloo::console, wasm_bindgen::prelude::*};
 
+async fn hello() {
+    use {serde::Serialize, tauri_wasm::Data};
+
+    #[derive(Serialize)]
+    struct Send {
+        message: &'static str,
+    }
+
+    let s = Send { message: "front!" };
+
+    match tauri_wasm::invoke_with_args("hello", Data(s)).await {
+        Ok(message) => console::log!("message from frontend received", message),
+        Err(e) => console::error!("failed to receive a message", e),
+    }
+}
+
+#[wasm_bindgen]
+pub async fn close() {
+    if let Err(e) = tauri_wasm::invoke("close").await {
+        console::error!("failed to close the application", e);
+    }
+}
+
 #[wasm_bindgen(start)]
-fn start() {
+async fn start() {
     if tauri_wasm::is_tauri() {
         console::log!("tauri was detected!");
     } else {
         console::warn!("tauri was not detected!");
     }
+
+    hello().await;
 }
